527e4252466e8f4b24cb5470f21985e92f6870fc
antonhann <antonha016@gmail.com>
2024-07-04 19:04:40 -0400

    added speed slider for the searching

--- src/Pages/Searching/Searching.tsx
+++ src/Pages/Searching/Searching.tsx
@@ -19,6 +19,8 @@ export const Searching = () => {
     const [animationOnGoing, setAnimationOnGoing] = useState<boolean>(false); 
     const [found, setFound] = useState<boolean>(false);
     const animationOnGoingRef = useRef(animationOnGoing); // To keep track of the animationOnGoing state
+    const [ms, setMs] = useState<number>(50); // speed of the animation
+    const slept = useRef(ms);
     const [foundIndex, setFoundIndex] = useState<number>(-1);
     const updateArray = async (newArray : Box[], time : number) => {
         //only update if there is an ongoing animation
@@ -26,6 +28,10 @@ export const Searching = () => {
             setArray([...newArray]);
             await sleep(time);
         }
+    }
+    const setSleptState = (time : number) => {
+        slept.current = time;
+        setMs(time);
     }
     // set the animationOnGoing state and update the ref
     const setAnimationOnGoingState = (state : boolean) => {
@@ -41,17 +47,16 @@ export const Searching = () => {
             colorBox(array, foundIndex, Color.defaultColor, 0);
         }
         let localArray : Box[]= [...array];
-        let slept = 50;
         setAnimationOnGoingState(true);
         for(let i : number = 0; i < localArray.length; i++){
-            await colorBox(localArray, i, Color.highlightColor, slept);
+            await colorBox(localArray, i, Color.highlightColor, slept.current);
             if(localArray[i].value === 5){
                 setFound(true);
                 setFoundIndex(i);
-                await colorBox(localArray, i, Color.doneColor, slept);
+                await colorBox(localArray, i, Color.doneColor, slept.current);
                 break;
             }
-            await colorBox(localArray, i, Color.defaultColor, slept);
+            await colorBox(localArray, i, Color.defaultColor, slept.current);
         }
     }
     const binarySearch = async () => {
@@ -59,41 +64,51 @@ export const Searching = () => {
             colorBox(array, foundIndex, Color.defaultColor, 0);
         }
         let localArray : Box[]= [...array];
-        let slept = 500;
         let l : number = 0;
         let r : number = localArray.length - 1;
         let target = 5;
-        colorBox(localArray, l, Color.highlightColor, slept);
-        await colorBox(localArray, r, Color.highlightColor, slept);
+        colorBox(localArray, l, Color.highlightColor, slept.current);
+        await colorBox(localArray, r, Color.highlightColor, slept.current);
         setAnimationOnGoingState(true);
         while (l <= r){
             let m : number = Math.floor((l + r) / 2);
-            await colorBox(localArray, m, Color.sortedColor, slept);
+            await colorBox(localArray, m, Color.sortedColor, slept.current);
             if(localArray[m].value === target){
-                colorBox(localArray, l, Color.defaultColor, slept);
-                colorBox(localArray, r, Color.defaultColor, slept);
+                colorBox(localArray, l, Color.defaultColor, slept.current);
+                colorBox(localArray, r, Color.defaultColor, slept.current);
                 setFound(true);
                 setFoundIndex(m);
-                await colorBox(localArray, m, Color.doneColor, slept);
+                await colorBox(localArray, m, Color.doneColor, slept.current);
                 break;
             }
             else if(localArray[m].value < target){
-                colorBox(localArray, l, Color.defaultColor, slept);
+                colorBox(localArray, l, Color.defaultColor, slept.current);
                 l = m + 1;
-                await colorBox(localArray, l, Color.highlightColor, slept);
+                await colorBox(localArray, l, Color.highlightColor, slept.current);
             }
             else{
-                colorBox(localArray, r, Color.defaultColor, slept);
+                colorBox(localArray, r, Color.defaultColor, slept.current);
                 r = m - 1;
-                await colorBox(localArray, r, Color.highlightColor, slept);
+                await colorBox(localArray, r, Color.highlightColor, slept.current);
             }
-            await colorBox(localArray, m, Color.defaultColor, slept);
+            await colorBox(localArray, m, Color.defaultColor, slept.current);
         }
     }
     return (
         <AppContainer>
             <div className="d-flex justify-content-around">
                   <button>Generate Array</button>
+                  <div className="d-flex gap-2">
+                        <label>Speed: {ms}ms</label>
+                        <input
+                            className="slider"
+                            type="range"
+                            min="0"
+                            max="1000"
+                            value={ms}
+                            onChange={(e) => setSleptState(parseInt(e.target.value))}
+                        />
+                    </div>
                   <div className="d-flex gap-2">
                       <button onClick={() => linearSearch()}>Linear Search</button>
                       <button onClick={() => binarySearch()}>Binary Search</button>
