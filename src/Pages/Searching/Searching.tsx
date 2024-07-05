4e37e932aa9d7cc5fd7c9f3bcf9883008bf4f789
antonhann <antonha016@gmail.com>
2024-07-04 21:39:38 -0400

    fixed import

--- src/Pages/Searching/Searching.tsx
+++ src/Pages/Searching/Searching.tsx
@@ -1,7 +1,6 @@
 import { useRef, useState } from "react";
 import { AppContainer, sleep } from "../helper";
 import { Color } from "../SortPage/Sort";
-import { color } from "d3";
 class Box{
     value: number;
     color: string;
